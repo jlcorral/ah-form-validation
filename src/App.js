a0b363a40923c6671de3d1423e4c181d544bccc3
Corral <jl.corralsoto@gmail.com>
2020-09-23 16:21:11 -0700

    validate forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import debounce from 'lodash.debounce';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [person, setPerson] = useState({name: 'Aida', lastName:'Valuenzuela'}); 
+  const [errors, setErrors] = useState({});
+
+  const getPersonFormErrors = (p) => {
+    const updatedErrors = {};
+    Object.keys(p).forEach(key=> {
+      console.log(p[key]);
+      if(p[key].trim() === '')
+        updatedErrors[key] = 'Required';
+    });
+
+    return updatedErrors;
+  };
+
+
+  const validate = useCallback(debounce((p) => setErrors(getPersonFormErrors(p)),250), [],);
+
+  const handleChange = ({target}) => {
+    setPerson((prev)=> ({...prev, [target.name]: target.value}));
+  }
+
+  useEffect(()=>{
+    validate(person);
+    return () => {
+      validate.cancel();
+    }
+  },[person])
+
+  console.log(errors);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField
+          error={!!errors['name']}
+          id="standard-error"
+          name="name"
+          label="Name"
+          value={person.name}
+          onChange={handleChange}
+        />
+        <TextField
+          error={!!errors['lastName']}
+          id="standard-error-helper-text"
+          name="lastName"
+          label="Last Name"
+          defaultValue={person.lastName}
+          helperText={errors['lastName']}
+          onChange={handleChange}
+        />
+      </div>
+    </form>
   );
 }
 
